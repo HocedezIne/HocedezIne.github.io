d6df30af9d07ea92bb415d9a2e6b091fe27ce6d6
HocedezIne <ine.hocedez@student.howest.be>
2020-12-09 13:35:46 +0100

    New Moon fixed

--- script/app.js
+++ script/app.js
@@ -106,8 +106,7 @@ const showResult = queryResponse => {
 			
 				// calculate data
 				moonRise = 1;
-			}
-			else {
+			} else {
 				// set time elements
 				document.querySelector(".js-moon-rise").innerText = queryResponse.moonRise;
 
@@ -134,6 +133,31 @@ const showResult = queryResponse => {
 			document.querySelector(".js-timeline-moon__set").style.gridColumn = `1 / span ${moonSet}`;
 			document.querySelector(".js-timeline-moon__set").style.justifyContent = "flex-end";
 			document.querySelector(".js-timeline-moon__rise").style.gridColumn = `${moonRise} / -1`;
+			break;
+		case "Waxing Crescent":
+			// add extra html
+			document.querySelector(".js-moon").innerHTML = `<div class="c-timeline__line js-timeline-moon"><time class="c-timeline__time js-moon-rise">__:__</time><time class="c-timeline__time js-moon-set">__:__</time></div>`;
+			
+			// set time elements
+			document.querySelector(".js-moon-rise").innerText = queryResponse.moonRise;
+			document.querySelector(".js-moon-set").innerText = queryResponse.moonSet;
+
+			// calculate data
+			moonRise = Math.round((parseInt(queryResponse.moonRise.split(":")[0]) * 60 + parseInt(queryResponse.moonRise.split(":")[1])) / 1440 * 100);
+			moonSet = Math.round((parseInt(queryResponse.moonSet.split(":")[0]) * 60 + parseInt(queryResponse.moonSet.split(":")[1])) / 1440 * 100);
+
+			// edit timeline
+			document.querySelector(".js-timeline-moon").style.gridColumn = `${moonRise} / ${moonSet}`;
+
+			if(queryResponse.moonUnder == null){
+				// the actual moon phase is new moon
+				document.querySelector(".js-moonContainer").innerHTML = `<svg class="c-moonphase__icon"><use xlink:href="#NewMoon"></use></svg>`;
+				document.querySelector(".js-moonPhase").innerText = "New Moon";
+				var phaseExplanationData = phaseExplanation["New Moon"];
+				document.querySelector(".js-rise-comparison").innerText = phaseExplanationData.rise;
+				document.querySelector(".js-transit-comparison").innerText = phaseExplanationData.transit;
+				document.querySelector(".js-set-comparison").innerText = phaseExplanationData.set;
+			}
 			break;
 		case "Waxing Gibbous":
 			// add extra html
@@ -184,7 +208,7 @@ const getAPI = async (position) => {
     const date = `${now.getFullYear()}${(now.getMonth()+1).toString().padStart(2,"0")}${now.getDate().toString().padStart(2,"0")}`;
     const timezoneOffset = now.getTimezoneOffset()*-1/60;
     
-    const url = `https://api.solunar.org/solunar/${lat},${lon},20201208,${timezoneOffset}`
+    const url = `https://api.solunar.org/solunar/${lat},${lon},20201214,${timezoneOffset}`
 
     const data = await fetch(url)
 					.then((r) => r.json())
